8863597bca94150613d95d6f66bd1357eb8dde04
josephcureton <josephcureton1@gmail.com>
2017-01-24 17:31:14 -0500

    Changes to the layout. Please review the images and changes in layout between mobile and desktop.

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,10 +4,12 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.tooltipped').tooltip({delay: 50});
+    $('.carousel.carousel-slider').carousel({full_width: true});
     $('.slider').slider({
     	full_width: true,
     	interval: 8000
     });
+    $('.parallax').parallax();
      $('.modal-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
@@ -17,6 +19,7 @@
       ending_top: '10%', // Ending top style attribute
 
     });
+    $('.chips').material_chip();
     $('ul.tabs').tabs();
      var options = [
        {selector: '#rule1', offset: 0, callback: function(el) {
@@ -29,7 +32,20 @@
       Materialize.fadeInImage($(el));
     } }
       ];
-      Materialize.scrollFire(options);
+    
+    Materialize.scrollFire(options);
+    $('select').material_select();
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 2 // Creates a dropdown of 15 years to control year
+    });
+    $('#modal1').on('keyup keypress', function(e) {
+      var keyCode = e.keyCode || e.which;
+      if (keyCode === 13) { 
+        e.preventDefault();
+        return false;
+      }
+    });
     $('#nav').Stickyfill();
     $(".button-collapse").sideNav()
   }); // end of document ready
